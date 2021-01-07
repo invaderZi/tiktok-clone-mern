0bc6cd91acffb1f1128ba8280e31fbd70684d844
Zi <aranzias@gmail.com>
2021-01-07 20:47:50 -0300

    video snap and moving with play and pause

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="app">
       <div className="app_videos">
         <Video />
+
         <Video />
       </div>
     </div>
--- src/Video.js
+++ src/Video.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Video.css";
 
 function Video() {
+  const [playing, setPlaying] = useState(false);
+
+  const videoReference = useRef(null);
+
+  const handleVideoPress = () => {
+    if (playing) {
+      videoReference.current.pause();
+      setPlaying(false);
+    } else {
+      videoReference.current.play();
+      setPlaying(true);
+    }
+  };
+
   return (
     <div className="video">
       <video
+        onClick={handleVideoPress}
         className="video_player"
         loop
+        ref={videoReference}
         src="https://player.vimeo.com/external/484412375.sd.mp4?s=52d106f697b09a7f11f4292beddc601969469728&profile_id=165&oauth2_token_id=57447761"
       ></video>
 
       {/*videofooter*/}
+
       {/*sidebar*/}
     </div>
   );
